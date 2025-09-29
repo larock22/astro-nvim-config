c5f74fae843d82a50fe16333e22ee18836344b64
ysosrs_83 <ysosrs_83@cooldev.git>
2025-09-29 16:07:32 -0500

    Enable cursorline for better cursor visibility
    Added cursorline = true to AstroCore options to highlight the current line, making it easier to track cursor position in the editor.

--- lua/plugins/astrocore.lua
+++ lua/plugins/astrocore.lua
@@ -44,6 +44,7 @@ return {
         spell = false, -- sets vim.opt.spell
         signcolumn = "yes", -- sets vim.opt.signcolumn to yes
         wrap = false, -- sets vim.opt.wrap
+        cursorline = true, -- highlight current line
       },
       g = { -- vim.g.<key>
         -- configure global vim variables (vim.g)
