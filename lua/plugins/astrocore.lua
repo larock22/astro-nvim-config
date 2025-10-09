5e34444eb02f729f00bde9e798fe6499907ca83f
ysosrs_83 <ysosrs_83@cooldev.git>
2025-10-09 15:00:32 -0500

    Enable text wrapping globally
    Changed wrap = false to wrap = true in astrocore.lua to fix text not wrapping in terminals and other windows.

--- lua/plugins/astrocore.lua
+++ lua/plugins/astrocore.lua
@@ -43,7 +43,7 @@ return {
         number = true, -- sets vim.opt.number
         spell = false, -- sets vim.opt.spell
         signcolumn = "yes", -- sets vim.opt.signcolumn to yes
-        wrap = false, -- sets vim.opt.wrap
+        wrap = true, -- sets vim.opt.wrap
         cursorline = true, -- highlight current line
       },
       g = { -- vim.g.<key>
